424c2a64e5a4693f1c4d5e7e0a1dc6eeb4f98af8
abhyaskumar9 <abhyaskumar9@gmail.com>
2026-09-08 15:33:08 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,21 +9,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// OFFICIAL PLUGINS VERSION CONFIGURATION: 
-// Yeh block bina kisi crash ke aapke sabhi host packages ko strict API 36 allocation provide karega
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            project.extensions.configure<com.android.build.api.dsl.LibraryExtension> {
-                compileSdk = 36
-                defaultConfig {
-                    minSdk = 21
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
