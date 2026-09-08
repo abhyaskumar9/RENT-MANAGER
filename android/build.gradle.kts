d6486b4bd86aaac201ba7c51986d835ccc253a84
abhyaskumar9 <abhyaskumar9@gmail.com>
2026-09-08 09:23:32 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,6 +9,26 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// FORCE PLUGINS SDK 36 (Bina kisi typing crash ke plugins checking criteria pass karne ka sahi tarika)
+subprojects {
+    afterEvaluate {
+        val extension = extensions.findByName("android")
+        if (extension != null) {
+            val extensionClass = extension::class.java.name
+            if (extensionClass.contains("LibraryExtension") || extensionClass.contains("ApplicationExtension")) {
+                try {
+                    // Script automation explicitly versions ko update kar degi
+                    val dslExtension = extension as com.android.build.api.dsl.CommonExtension<*, *, *, *, *, *>
+                    dslExtension.compileSdk = 36
+                    dslExtension.defaultConfig.targetSdk = 36
+                } catch (e: Exception) {
+                    // Errors ko catch karke pipeline fail hone se rokega
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
