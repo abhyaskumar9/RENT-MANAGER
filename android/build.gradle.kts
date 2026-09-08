2f7703b45e00b2b7bdcf7e79163dfaaebb4cde90
abhyaskumar9 <abhyaskumar9@gmail.com>
2026-09-08 08:26:22 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,21 +7,6 @@ allprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
-    
-    plugins.withId("com.android.application") {
-        configure<com.android.build.api.dsl.ApplicationExtension> {
-            compileSdk = 34
-            defaultConfig {
-            }
-        }
-    }
-    plugins.withId("com.android.library") {
-        configure<com.android.build.api.dsl.LibraryExtension> {
-            compileSdk = 34
-            defaultConfig {
-            }
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
