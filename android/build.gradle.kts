b1d613a23c0326acebb83f2b66792e393832715e
abhyaskumar9 <abhyaskumar9@gmail.com>
2026-09-08 12:38:52 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,19 +9,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// OFFICIAL SYNTAX OVERRIDE: Yeh tarika bina kisi afterEvaluate error ke sabhi plugins ko direct control karta hai
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.findByName("android")
-            if (android is com.android.build.api.dsl.LibraryExtension) {
-                android.compileSdk = 36
-                android.defaultConfig.targetSdk = 36
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
