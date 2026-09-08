f2c4bb9eb68141fa342ceee10f749d3281c50083
abhyaskumar9 <abhyaskumar9@gmail.com>
2026-09-08 11:53:31 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,10 +5,31 @@ allprojects {
     }
 }
 
+rootProject.buildDir = '../build'
 subprojects {
-    project.evaluationDependsOn(":app")
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
+}
+subprojects {
+    project.evaluationDependsOn(':app')
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+// OFFICIAL FORCED SDK OVERRIDE (Bina kisi syntax error ke sabhi plugins ko patch karne ka standard tarika)
+subprojects {
+    afterEvaluate { project ->
+        if (project.hasProperty('android')) {
+            project.android {
+                if (namespace == null) {
+                    // Agar koi purana plugin bina namespace ke ho
+                }
+                compileSdkVersion 36
+                defaultConfig {
+                    targetSdkVersion 36
+                }
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
